8e0069fb7ef394a502be9df136feba545416576d
Gurmeet shimar <shimargunjan@gmail.com>
2025-12-22 19:56:28 +0530

    finding issues

--- gemini.js
+++ gemini.js
@@ -1,6 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { GoogleGenerativeAI } from "@google/generative-ai";
+console.log(
+  "VERCEL:", !!process.env.VERCEL,
+  "GEMINI KEY:", !!process.env.GEMINI_API_KEY
+);
 
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
